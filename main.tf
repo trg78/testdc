e019643daba90c3107fe1fce48260f4cb1603b13
trg78 <gorbenko.taras@gmail.com>
2020-04-03 17:35:21 +0300

    3 commit

--- main.tf
+++ main.tf
@@ -28,3 +28,16 @@ resource "digitalocean_floating_ip_assignment" "vm1" {
   ip_address = digitalocean_floating_ip.floating_ip.ip_address
   droplet_id = digitalocean_droplet.vm1.id
 }
+
+resource "digitalocean_domain" "vm1" {
+  name       = "devops-test.gomage.com"
+  ip_address = "${digitalocean_droplet.vm1.ipv4_address}"
+}
+
+resource "digitalocean_record" "vm1" {
+  domain = "${digitalocean_domain.vm1.name}"
+  type   = "A"
+  name   = "vm1"
+  value  = "${digitalocean_droplet.vm1.ipv4_address}"
+}
+
